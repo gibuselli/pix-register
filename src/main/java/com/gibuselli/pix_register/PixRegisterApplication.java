492d3facdd143044b89d09ab97a1e211227ab958
giovani <gibuselli@gmail.com>
2024-08-25 17:54:11 -0300

    feat: create spring boot project

---
+++ src/main/java/com/gibuselli/pix_register/PixRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.gibuselli.pix_register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gibuselli/pix_register/PixRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gibuselli.pix_register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
